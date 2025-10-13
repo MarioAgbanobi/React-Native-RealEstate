4b0e0c1acc97bed49cbe98fe0f75eb41141c5a00
MarioAgbanobi <marioagbanobi@gmail.com>
2025-10-13 01:40:34 +0100

    Nativewind setup

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -10,6 +10,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
+      <Text className="font-bold text-lg my-3">Welcome Screen</Text>
       <Link href="/sign-in" >
           Sign in
       </Link>
