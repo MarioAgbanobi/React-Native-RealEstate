655b804340cd3c88f0460185d97b90615844a496
MarioAgbanobi <marioagbanobi@gmail.com>
2025-10-20 22:47:11 +0100

    Designed home screen and Created seatch component

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,29 +1,43 @@
-import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import Search from "@/components/Search";
+import icons from "@/constants/icons";
+import images from "@/constants/images";
+import { Image, Text, TouchableOpacity, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text className="font-rubik-light text-3xl my-3">Welcome Real Esteem</Text>
-      <Link href="/sign-in" >
-          Sign in
-      </Link>
-      <Link href="/explore" >
-          Explore
-      </Link>
-      <Link href="/profile" >
-          Profile
-      </Link>
-      <Link href="/properties/[id]" >
-          Property
-      </Link>
-      
-    </View> 
+    <SafeAreaView className="bg-white h-full">
+      <View className="px-5">
+        
+        <View className="flex flex-row items-center justify-between mt-5">
+          <View className="flex flex-row items-center">
+            <Image source={images.avatar} className="size-12 rounded-full" />
+
+            <View className="flex flex-col items-start ml-2 justify-center">
+              <Text className="font-rubik-medium text-black-100 text-xs">
+                Good Morning
+              </Text>
+              <Text className="font-rubik-medium text-black-300 text-base">
+                Mario Agbanobi
+              </Text>
+            </View>
+          </View>
+
+          <Image source={icons.bell} className="size-6" />
+        </View>
+
+        <Search />
+
+        <View className="flex flex-row items-center justify-between">
+          <Text className="font-rubik-bold text-xl">Featured</Text>
+          <TouchableOpacity>
+            <Text className="text-base font-rubik-bold text-primary-300">
+              See All
+            </Text>
+          </TouchableOpacity>
+        </View>
+
+      </View>
+    </SafeAreaView>
   );
 }
