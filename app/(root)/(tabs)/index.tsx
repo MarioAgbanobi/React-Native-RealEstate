86cec6b2dd5d1d939dca584ddc4cb7bb88136030
MarioAgbanobi <marioagbanobi@gmail.com>
2025-10-20 23:38:08 +0100

    Created Cards component and Modified home screen

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,43 +1,68 @@
+import { FeaturedCard, PostCard } from "@/components/Cards";
 import Search from "@/components/Search";
 import icons from "@/constants/icons";
 import images from "@/constants/images";
-import { Image, Text, TouchableOpacity, View } from "react-native";
+import { Image, ScrollView, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
     <SafeAreaView className="bg-white h-full">
-      <View className="px-5">
-        
-        <View className="flex flex-row items-center justify-between mt-5">
-          <View className="flex flex-row items-center">
-            <Image source={images.avatar} className="size-12 rounded-full" />
+      <ScrollView>
+        <View className="px-5">
 
-            <View className="flex flex-col items-start ml-2 justify-center">
-              <Text className="font-rubik-medium text-black-100 text-xs">
-                Good Morning
-              </Text>
-              <Text className="font-rubik-medium text-black-300 text-base">
-                Mario Agbanobi
-              </Text>
+          {/* Top Nav */}
+          <View className="flex flex-row items-center justify-between mt-5">
+            <View className="flex flex-row items-center">
+              <Image source={images.avatar} className="size-12 rounded-full" />
+
+              <View className="flex flex-col items-start ml-2 justify-center">
+                <Text className="font-rubik-medium text-black-100 text-xs">
+                  Good Morning
+                </Text>
+                <Text className="font-rubik-medium text-black-300 text-base">
+                  Mario Agbanobi
+                </Text>
+              </View>
             </View>
+
+            <Image source={icons.bell} className="size-6" />
           </View>
 
-          <Image source={icons.bell} className="size-6" />
+          {/* Search */}
+          <Search />
+
+          {/* FeaturedCard */}
+          <View className="my-5">
+            <View className="flex flex-row items-center justify-between">
+              <Text className="font-rubik-bold text-xl">Featured</Text>
+              <TouchableOpacity>
+                <Text className="text-base font-rubik-bold text-primary-300">
+                  See All
+                </Text>
+              </TouchableOpacity>
+            </View>
+
+            <FeaturedCard />
+          </View>
+
+          {/* PostCard */}
+          <View className="mt-5 mb-20">
+            <View className="flex flex-row items-center justify-between">
+              <Text className="font-rubik-bold text-xl">
+                Our Recommendation
+              </Text>
+              <TouchableOpacity>
+                <Text className="text-base font-rubik-bold text-primary-300">
+                  See All
+                </Text>
+              </TouchableOpacity>
+            </View>
+
+            <PostCard />
+          </View>
         </View>
-
-        <Search />
-
-        <View className="flex flex-row items-center justify-between">
-          <Text className="font-rubik-bold text-xl">Featured</Text>
-          <TouchableOpacity>
-            <Text className="text-base font-rubik-bold text-primary-300">
-              See All
-            </Text>
-          </TouchableOpacity>
-        </View>
-
-      </View>
+      </ScrollView>
     </SafeAreaView>
   );
 }
